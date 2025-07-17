ce00e3b7230b4bc28644a0a66ded3a7ec8622d24
Shafayat Hasnat Rubaiyat <121631303+Infi9itee@users.noreply.github.com>
2025-07-17 17:44:44 +0600

    Update intro.c

--- intro.c
+++ intro.c
@@ -4,6 +4,6 @@ int main()
 {
 printf("I'm Rowza. A noob developer, And uploaded my noob CV here");
 printf("I've created a C program file to find the freaquency in an array in main branch of this repository");
-
+printf("I am here just to be here");
 return 0;
 {
