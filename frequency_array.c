bf6b00d2f79949385feda839dda147858524d636
Shafayat Hasnat Rubaiyat <121631303+Infi9itee@users.noreply.github.com>
2025-07-17 18:26:23 +0600

    Removed unneceassry if/else conditions

--- frequency_array.c
+++ frequency_array.c
@@ -12,24 +12,25 @@ int main()
     int freq[6] = {0}; // array initialization = {0} ekta 0 dilei shobgulo 0 diye initialize hye jay
 
     for(int i=0; i<n; i++){
-        if( a[i] == 0){
-            freq[0]++;
-        }
-        else if( a[i] == 1){
-            freq[1]++;
-        }
-        else if( a[i] == 2){
-            freq[2]++;
-        }
-        else if( a[i] == 3){
-            freq[3]++;
-        }
-        else if( a[i] == 4){
-            freq[4]++;
-        }
-        else if( a[i] == 5){
-            freq[5]++;
-        }
+        freq[a[i]]++;
+        // if( a[i] == 0){  // eto kisu korar dorkar ki freq[a[i]]++ korlei to hoi.
+        //     freq[0]++;
+        // }
+        // else if( a[i] == 1){
+        //     freq[1]++;
+        // }
+        // else if( a[i] == 2){
+        //     freq[2]++;
+        // }
+        // else if( a[i] == 3){
+        //     freq[3]++;
+        // }
+        // else if( a[i] == 4){
+        //     freq[4]++;
+        // }
+        // else if( a[i] == 5){
+        //     freq[5]++;
+        // }
     }
 
     printf("Frequency of 0 is %d -> %d\n", 0, freq[0]);
