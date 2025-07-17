3e0dee25986d2a3c9fd23b7b015ffc87b7515187
Fayroz Tasnim Rowza <190097630+napaSyrup-r@users.noreply.github.com>
2025-07-17 17:40:57 +0600

    Update intro.c
    Print message changed

--- intro.c
+++ intro.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-printf("I'm Rowza");
+printf("I'm Rowza. A noob developer, And uploaded my noob CV here");
 printf("I've created a C program file to find the freaquency in an array in main branch of this repository");
 
 return 0;
