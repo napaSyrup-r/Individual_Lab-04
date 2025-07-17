2da615aebe5db4950931d30427900b6582e1cd83
Fayroz Tasnim Rowza <190097630+napaSyrup-r@users.noreply.github.com>
2025-07-17 16:47:46 +0600

    Create intro.c
    1.  This is a C file of introduction.

---
+++ intro.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+printf("I'm Rowza");
+printf("I've created a C program file to find the freaquency in an array in main branch of this repository");
+
+return 0;
+{
